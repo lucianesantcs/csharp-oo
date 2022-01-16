27ebbf2b94649661fd5343eb109e79436647c279
Luciane Santos <lucianesantcs@gmail.com>
2022-01-16 18:54:24 -0300

    feat: variaveis double

---
+++ c-1/2-variablesDouble/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2_variablesDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criando variáveis ponto flutuante");
+
+            double salario;
+            salario = 1200.70;
+            Console.WriteLine("O salário é " + salario);
+
+            double idade;
+            idade = 15.0 / 2.0;
+            Console.WriteLine("Idade é " + idade);
+
+        }
+    }
+}
