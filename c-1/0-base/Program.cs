683ad6c3457bf240e7e3e2e8887db19d4a403f88
Luciane Santos <lucianesantcs@gmail.com>
2022-01-16 18:25:14 -0300

    feat: adicionado projeto base e variaveis

---
+++ c-1/0-base/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace C1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("A execução acabou. Tecle enter para finalizar.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ c-1/1-variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Criando variáveis");
+
+            int idade;
+            idade = 32;
+
+            Console.WriteLine("Sua idade é " + idade);
+        
+            idade = 10 + 5;
+
+            Console.WriteLine("Soma de 10 + 5 é " + idade);
+        }
+    }
+}
