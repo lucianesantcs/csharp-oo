2be2187969bc35b23dde1a90e3f0093d311142ee
Luciane Santos <lucianesantcs@gmail.com>
2022-01-20 19:53:26 -0300

    feat: caracteres e textos

---
+++ c-1/5-caracteresTextos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5_caracteresTextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Caracteres e textos");
+            
+            char primeiraLetra = 'a';
+            primeiraLetra = (char)65; // retorna A
+            primeiraLetra = (char)(primeiraLetra + 1); // retorna B
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura cursos de tecnologia " + 2022;
+            Console.WriteLine(titulo);
+
+            // string cursosProgramação = " - .NET" + " - Java" + " - JavaScript";
+            string cursosProgramação = 
+            @" - .NET 
+            - Java 
+            - JavaScript";
+            Console.WriteLine(cursosProgramação);
+        }
+    }
+}
