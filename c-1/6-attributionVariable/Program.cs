656617284183cce54f2f3ab7adb77f5e730e31e5
Luciane Santos <lucianesantcs@gmail.com>
2022-01-20 20:09:01 -0300

    feat: atribuiçao de variaveis

---
+++ c-1/6-attributionVariable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _6_attributionVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Atribuições de variáveis");
+            int idade = 32;
+            int idadeGustavo = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade); // 20
+            Console.WriteLine(idadeGustavo); // 32
+
+        }
+    }
+}
