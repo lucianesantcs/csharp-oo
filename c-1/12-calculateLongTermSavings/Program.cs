b6308b7e34335583ce0d4a1b9e53e4df9d25695d
Luciane Santos <lucianesantcs@gmail.com>
2022-01-22 15:46:34 -0300

    feat: calcula poupança longo prazo com for

---
+++ c-1/12-calculateLongTermSavings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _12_calculateLongTermSavings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular poupança longo prazo");
+
+            double valorInvestido = 1000;
+            double fatorRendimento = 1.0036;
+
+            for(int contadorAno = 1; contadorAno <= 5; contadorAno++) 
+            {   
+                for(int contadorMes = 1; contadorMes <= 12; contadorMes++) 
+                {
+                    valorInvestido *= fatorRendimento;
+                }
+                fatorRendimento += 0.0010;
+            }
+
+            Console.WriteLine("Ao término do investimento, você terá R$" + valorInvestido);
+        }
+    }
+}
