9ce258b9fe5260bd1023e8164af40d073e397a43
Luciane Santos <lucianesantcs@gmail.com>
2022-01-22 16:06:29 -0300

    feat: for encadeado contador

---
+++ c-1/13-chainedFor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _13_chainedFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For encadeado");
+
+            for(int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    if(contadorColuna > contadorLinha)
+                    {
+                        break;
+                    }
+                    Console.Write(contadorColuna + 1);
+                }
+
+                Console.WriteLine(); // pula linha
+            }
+
+
+            // for(int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            // {
+            //     for(int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+            //         Console.Write("*");
+
+            //     Console.WriteLine(); // pula linha
+            // }
+
+
+            // for(int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            // {
+            //     for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+            //     {
+            //         Console.Write("*");
+            //         if(contadorColuna >= contadorLinha)
+            //             break;
+            //     }
+
+            //     Console.WriteLine(); // pula linha
+            // }
+        }
+    }
+}
