5742c4d6a26010bcc773646603020ddfb683bf6c
Luciane Santos <lucianesantcs@gmail.com>
2022-01-22 15:35:37 -0300

    feat: estrutura de repetiçao for

---
+++ c-1/11-calculateSavingsFor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _10_calculateSavings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular poupança com for");
+
+            double valorInvestido = 1000;
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
+            }
+        }
+    }
+}
