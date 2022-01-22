17b3971ebb08f71e36c0231f326ce3233c9e678f
Luciane Santos <lucianesantcs@gmail.com>
2022-01-22 14:28:18 -0300

    feat: condicional boolean

---
+++ c-1/8-conditionalBoolean/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _7_conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Condicional boolean");
+
+            int idadeJoao = 16;
+            // int quantidadePessoas = 2;
+            // bool acompanhado = quantidadePessoas >= 2;
+            bool acompanhado = false;
+
+            if(idadeJoao >= 18 || acompanhado)
+            {
+                Console.WriteLine("Pode entrar.");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar.");
+            }
+        }
+    }
+}
