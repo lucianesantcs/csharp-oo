4bf4804c417294e507b4f01b3223e4e7cc6a45b4
Luciane Santos <lucianesantcs@gmail.com>
2022-01-22 16:13:45 -0300

    feat: tabuada com for

---
+++ c-1/14-multiplicationTable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _14_multiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada com for");
+
+            for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+            {
+                for (int contador = 0; contador <= 10; contador++)
+                {
+                    Console.Write(multiplicador + " * " + contador + " = " + multiplicador * contador);
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
