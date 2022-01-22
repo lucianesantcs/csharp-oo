92e0ed4a413c653763f13b28f28c9c4860819b07
Luciane Santos <lucianesantcs@gmail.com>
2022-01-22 14:16:05 -0300

    feat: condicional if

---
+++ c-1/7-conditionals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _7_conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Condicionais");
+
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+
+            if(idadeJoao >= 18)
+            {
+                Console.WriteLine("Possui mais de 18 anos.");
+            }
+            else
+            {
+                if(quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("Não possui mais de 18, mas está acompanhado.");
+                }
+                else
+                {
+                    Console.WriteLine("Não possui mais de 18 anos.");
+                }
+            }
+        }
+    }
+}
