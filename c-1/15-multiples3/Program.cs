ccf050bfa8c19a0e6d6ea5939313ecdcb2ea3f20
Luciane Santos <lucianesantcs@gmail.com>
2022-01-24 08:28:50 -0300

    feat: multiplos de 3

---
+++ c-1/15-multiples3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _15_multiples3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Múltiplos de 3");
+
+            for(int contador = 1; contador <= 100; contador++)
+            {
+                if(contador % 3 == 0)
+                {
+                    Console.Write(contador + " ");
+                }
+            }
+        }
+    }
+}
