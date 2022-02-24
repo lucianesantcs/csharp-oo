d0a9254e5384ed2bdebff6b307c76dcaf5e7a8ac
Luciane Santos <lucianesantcs@gmail.com>
2022-02-24 16:32:50 -0300

    feat: criado classe com propriedades e tipagem correta

---
+++ csharp-2/01-first-class/Account.cs
@@ -0,0 +1,7 @@
+public class ContaCorrente 
+{
+  public string titular;
+  public int numeroAgencia;
+  public int numero;
+  public double saldo;
+}
---
+++ csharp-2/01-first-class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01_first_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // separado objeto em uma classe Account.cs
+            // string titular = "Gabriela";
+            // int numeroAgencia = 863;
+            // int numero = 863146;
+            // double saldo = 100;
+
+            ContaCorrente contaGabriela = new ContaCorrente();
+            contaGabriela.titular = "Gabriela";
+            contaGabriela.numeroAgencia = 863;
+            contaGabriela.numero = 863146;
+            contaGabriela.saldo = 100;
+
+            contaGabriela.saldo += 200;
+
+            Console.WriteLine(contaGabriela.titular);
+            Console.WriteLine(contaGabriela.saldo);
+        }
+    }
+}
