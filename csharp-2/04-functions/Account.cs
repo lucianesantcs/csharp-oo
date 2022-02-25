e651f43048368afa4788fc425445a8b40ee124f7
Luciane Santos <lucianesantcs@gmail.com>
2022-02-25 13:44:39 -0300

    feat: criado funçoes/metodos na classe

---
+++ csharp-2/04-functions/Account.cs
@@ -0,0 +1,37 @@
+public class ContaCorrente 
+{
+  public string titular;
+  public int numeroAgencia;
+  public int numero;
+  public double saldo = 100;
+
+  public bool Sacar(double valor) 
+  {
+    if(this.saldo < valor) 
+    {
+      return false;
+    }
+
+    this.saldo -= valor;
+    return true;
+  }
+
+  public void Depositar(double valor)
+  {
+    this.saldo += valor;
+  }
+
+  public bool Transferir(double valor, ContaCorrente contaDestino) 
+  {
+    if(this.saldo < valor) 
+    {
+      return false;
+    }
+
+    this.saldo -= valor;
+    // contaDestino.saldo += valor;
+    contaDestino.Depositar(valor);
+    return true;
+  }
+
+}
---
+++ csharp-2/04-functions/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04_functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+            contaDoBruno.titular = "Bruno";
+
+            // contaDoBruno.Sacar(50);
+            bool resultadoSaque = contaDoBruno.Sacar(50);
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoSaque);
+
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+
+            contaDoBruno.Transferir(200, contaDaGabriela);
+            bool resultadoTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine(contaDaGabriela.saldo); // 500
+            Console.WriteLine(contaDoBruno.saldo); // 150
+            Console.WriteLine(resultadoTransferencia); // true
+        }
+    }
+}
