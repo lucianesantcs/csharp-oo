c83d3d523b02d38d83ec854e245bcef2dfe6c16f
Luciane Santos <lucianesantcs@gmail.com>
2022-02-25 11:29:19 -0300

    feat: valores padroes na classe

---
+++ csharp-2/02-standard-values/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02_standard_values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+            conta.titular = "Gabriela";
+            
+            Console.WriteLine(conta.saldo); // retorna 100 definido na classe Account
+        }
+    }
+}
