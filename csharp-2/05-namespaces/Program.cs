06a821fca6e81bba2e7a75a07d87eca2193536d0
Luciane Santos <lucianesantcs@gmail.com>
2022-03-10 16:41:36 -0300

    feat: namespaces e referencias

---
+++ csharp-2/05-namespaces/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Client gabriela = new Client();
+            // gabriela.nome = "Gabriela";
+            // gabriela.profissao = "Desenvolvedora C#";
+            // gabriela.cpf = "434.568.878-20";
+
+            ContaCorrente conta = new ContaCorrente();
+            // conta.titular = gabriela;
+            conta.saldo = 500;
+            conta.numeroAgencia = 563;
+            conta.numero = 5634527;
+            
+            // 2 maneira de instanciar um novo cliente
+            conta.titular = new Client();
+            conta.titular.nome = "Gabriela Costa";
+            conta.titular.profissao = "Desenvolvedora C#";
+            conta.titular.cpf = "434.568.878-20";
+
+
+            // Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+        }
+    }
+}
